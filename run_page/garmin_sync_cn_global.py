15705753057b629cf2b0f6002bb3b1b88aec284c
Proming <promingx@gmail.com>
2024-04-11 11:48:58 +0800

    fix bug

--- run_page/garmin_sync_cn_global.py
+++ run_page/garmin_sync_cn_global.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     to_upload_files = []
     if new_ids is None:
         new_ids = set()
-    new_ids = new_ids.update(unsynced_ids)
+    new_ids.update(unsynced_ids)
     for i in new_ids:
         if os.path.exists(os.path.join(FIT_FOLDER, f"{i}.fit")):
             # upload fit files
